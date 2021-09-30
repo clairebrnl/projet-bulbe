849b0fbcf608abe164852838dee6048b5c1033cf
cyril cabrolier <cyr.cab@hotmail.fr>
2021-09-30 11:21:04 +0200

    le sous menu s'ouvre quand on clique dessus

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const buttonToActive = document.querySelector('.button-menu');
-const navMenu = document.querySelector('.nav-list');
+const buttonToActive = document.querySelector(".button-menu");
+const navMenu = document.querySelector(".nav-list");
+const hidden = document.querySelector(".sous-menu");
+const activeHidden = document.querySelector(".deploy")
 
-buttonToActive.addEventListener('click', function () {
-    navMenu.classList.toggle('active');
-})
+buttonToActive.addEventListener("click", function () {
+  navMenu.classList.toggle("active");
+});
+
+activeHidden.addEventListener("click", function () {
+  hidden.classList.toggle("active-sousMenu");
+});
