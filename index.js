8ba70a6d842bf3f4dcf7aa625da1abba9dab9765
cyril cabrolier <cyr.cab@hotmail.fr>
2021-10-01 11:05:02 +0200

    initialisation du sous menu sur la version desktop

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+//modification du dom pour la verison desktop
 const hidden = document.querySelector(".sous-menu");
 const activeHidden = document.querySelector(".deploy");
-
-activeHidden.addEventListener("click", function () {
+activeHidden.addEventListener("mouseover", function () {
   hidden.classList.toggle("active-sousMenu");
 });
+hidden.addEventListener("mouseover", function() {
+  hidden.classList.toggle("active-sousMenu");
+})
+hidden.classList.toggle("active-sousMenu");
+
+
