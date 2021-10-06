247931ca0f1dc5f799987376ecb46d1153daf7e0
cyril cabrolier <cyr.cab@hotmail.fr>
2021-10-06 10:53:07 +0200

    modifs nav dom

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 //modification du dom pour la verison desktop
-const hidden = document.querySelector(".sous-menu");
-const activeHidden = document.querySelector(".deploy");
-activeHidden.addEventListener("mouseover", function () {
-  hidden.classList.toggle("active-sousMenu");
-});
-hidden.addEventListener("mouseover", function() {
-  hidden.classList.toggle("active-sousMenu");
+const listLink = document.querySelectorAll('.list-link');
+const lien = document.querySelector('.active-menu')
+const hiddenMenu = document.querySelector('.sous-menu')
+
+document.body.addEventListener('mouseover', function(event) {
+  console.log(event.target);
+  if (event.target == lien || event.target == listLink) {
+    console.log("j'active");
+    hiddenMenu.classList.add('active-sousMenu');
+   }
+  else {
+    console.log("j'efface");
+    hiddenMenu.classList.remove('active-sousMenu')
+  }
 })
-hidden.classList.toggle("active-sousMenu");
+
 
 
