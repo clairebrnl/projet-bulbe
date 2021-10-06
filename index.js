54a3ed5bb1de0b3e5c9261aaec036eab46018804
Laurie Pujos <pujos.laurie@gmail.com>
2021-10-06 12:53:33 +0200

    ajout index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 //modification du dom pour la verison desktop
-<<<<<<< HEAD
 const hidden = document.querySelector(".sous-menu");
 const activeHidden = document.querySelector(".deploy");
 activeHidden.addEventListener("mouseover", function () {
@@ -8,8 +7,6 @@ activeHidden.addEventListener("mouseover", function () {
 hidden.addEventListener("mouseover", function() {
   hidden.classList.toggle("active-sousMenu");
 })
-=======
->>>>>>> e28c037a1f1c5b6f0f71ca79ee359be0185e1658
 
 
 
