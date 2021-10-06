82fa836e9da15d94b50c7a30f7be7e51aecef355
Yohann Mahrez <yohann.mahrez@gmail.com>
2021-10-06 12:14:52 +0200

    Ajout bouton newsletter + index.js fenetre popup

--- index.js
+++ index.js
@@ -9,6 +9,27 @@ hidden.addEventListener("mouseover", function() {
 })
 hidden.classList.toggle("active-sousMenu");
 
+//---------------Button newsletter----
+
+const btnPopup = document.getElementById('btnPopup');
+const overlay = document.getElementById('overlay');
+const btnClose = document.getElementById('btnClose');
+
+btnPopup.addEventListener('click', openModal);
+
+btnClose.addEventListener('click', closePopup);
+
+function openModal (){
+  overlay.style.display = "block";
+}
+
+function closePopup(){
+  overlay.style.display='none';
+}
+
+
+
+
 
 
 
