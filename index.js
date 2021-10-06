1a3e8f0e7ac48e33f0438e7bc60232b10d9ce531
cyril cabrolier <cyr.cab@hotmail.fr>
2021-10-06 12:00:55 +0200

    suppression des fonctions js de manipulation de DOM et simplification en CSS

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
 //modification du dom pour la verison desktop
-const listLink = document.querySelectorAll('.list-link');
-const lien = document.querySelector('.active-menu')
-const hiddenMenu = document.querySelector('.sous-menu')
-
-document.body.addEventListener('mouseover', function(event) {
-  console.log(event.target);
-  if (event.target == lien || event.target == listLink) {
-    console.log("j'active");
-    hiddenMenu.classList.add('active-sousMenu');
-   }
-  else {
-    console.log("j'efface");
-    hiddenMenu.classList.remove('active-sousMenu')
-  }
-})
 
 
 
